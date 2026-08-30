876c2f2a584f4599d634fad2ee045e4ce71af30f
Cameron Asselin <100627085+CameronAsselin@users.noreply.github.com>
2026-08-30 17:44:19 -0400

    Added theharvester

--- setup.sh
+++ setup.sh
@@ -73,7 +73,7 @@ cd paru
 makepkg -si
 cd
 sudo rm -r -f ~/Downloads/paru
-paru -S ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-ubuntu-font-family chili-sddm-theme papirus-folders-catppuccin-git tuxguitar
+paru -S ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-ubuntu-font-family chili-sddm-theme papirus-folders-catppuccin-git theharvester-git tuxguitar
 
 # GRUB theme
 sudo grub-mkconfig -o /boot/grub/grub.cfg
