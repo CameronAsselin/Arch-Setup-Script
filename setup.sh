fa277e78e77276899630e0943c82f6aab7ae8c5b
Cameron Asselin <100627085+CameronAsselin@users.noreply.github.com>
2026-09-03 15:23:44 -0400

    Commented out chili-sddm-theme

--- setup.sh
+++ setup.sh
@@ -78,7 +78,7 @@ cd paru
 makepkg -si
 cd
 sudo rm -r -f ~/Downloads/paru
-paru -S ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-ubuntu-font-family chili-sddm-theme papirus-folders-catppuccin-git
+paru -S ttf-freefont ttf-ms-fonts ttf-linux-libertine ttf-dejavu ttf-ubuntu-font-family papirus-folders-catppuccin-git #chili-sddm-theme
 
 # GRUB theme
 sudo grub-mkconfig -o /boot/grub/grub.cfg
